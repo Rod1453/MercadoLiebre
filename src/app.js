43425a7878eda5df0a4430cfbf0ab3d492282b06
Rod1453 <rodrielpanadou@gmail.com>
2023-09-02 00:59:50 -0300

    act

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
+app.use(express.static('public'));
 
-app.use(express.static("public"));
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => "servidor escuchando en el puerto 3000");
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/home.html"))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
 });
 
-app.get("/registro", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/register.html"))
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
 });
 
-app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/login.html"))
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`PORT: ${PORT}`);
 });
